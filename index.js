42aea032b521ddc981d456fa0570208a7170b3ee
Akshad-codes <akshadkulkarni503@gmail.com>
2026-02-18 07:29:09 +0000

    Add passenger-coungter files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+
+    saveEl.textContent += countStr 
+    console.log(count)
+    countEl.innerText=0
+    count=0
+}            
+    // Here textContent is used instead of innertext cuz //
+    // the there is problem occuring with "-" as it is not //
+    //human readable and innertext is useful for human readable content//
+
+    
